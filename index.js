4d928a90b2c622c0e2dc1bb94dee4c05492756e8
Jon Robeson <jcbrobeson@gmail.com>
2025-07-14 16:04:38 -0500

    config

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import summaryRouter from './routes/summarize.js';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express();
 const PORT = process.env.port || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 
 app.get('/', (_, res) => {
-	res.sendFile(path.join(__dirname, './index.html'));
+	res.sendFile(join(__dirname, './index.html'));
 });
 
 app.use('/api/summary', summaryRouter);

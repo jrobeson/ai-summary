2765c51a25065d77a03d312b7fb5b587205048d4
Jon Robeson <jcbrobeson@gmail.com>
2025-07-14 15:23:00 -0500

    .

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import summaryRouter from '../routes/summarize.js';
 
 const app = express();
+const PORT = process.env.port || 3000;
 
 app.use(express.json());
 
 app.use('/api/summary', summaryRouter);
 
-app.listen(3000, () => console.log('AI Summary service running'));
+app.listen(PORT, () => console.log('AI Summary service running'));
